9c73328fad7f15d120c46a3721b569df8a4a2964
Suehyun666 <em0200878@gmail.com>
2025-11-19 13:14:02 +0900

    init(project): main 함수 및 test

---
+++ src/main/java/org/board/board_be/BoardBeApplication.java
@@ -0,0 +1,13 @@
+package org.board.board_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/board/board_be/BoardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.board.board_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
