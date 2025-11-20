7d788583d052ee08fbaad8a6cd6be389a7244d6f
Suehyun666 <em0200878@gmail.com>
2025-11-20 13:33:56 +0900

    feat(openai): openapi 버전 명시

--- src/main/java/org/board/board_be/config/OpenApiConfig.java
+++ src/main/java/org/board/board_be/config/OpenApiConfig.java
@@ -17,6 +17,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .openapi("3.0.0")
                 .info(new Info()
                         .title("Board API")
                         .description("""
